de22fbd26223f658ff1a71e8c2ca1524d587f5ae
Cyan Tarek <cyantarek.cg@gmail.com>
2018-01-29 00:56:07 +0600

    heroku_config

--- Sixxer/settings.py
+++ Sixxer/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-				'social.apps.django_app.context_processors.backends',
+				'social_django.context_processors.backends',
 				'social_django.context_processors.login_redirect',
             ],
         },
